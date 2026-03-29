39d2378f6eaabf60a54cc93ddc0c8ee66e6d19ee
IIxIIIxII <talipova_kamilla@iuca.kg>
2026-03-29 20:31:21 +0600

    Update email settings to use Gmail SMTP and adjust TLS/SSL configurations

--- cosmic_game/settings.py
+++ cosmic_game/settings.py
@@ -92,12 +92,15 @@ USE_TZ = True
 
 # --- Почта---
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
-EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'False') == 'True'
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 465))
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
+
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'False') == 'True'
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'True') == 'True'
+
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
